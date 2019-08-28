c478fc81fdb525ef9c19f8d683f24a3631b49f16
dbp <dbp@DBP.(none)>
2019-08-28 02:48:48 +0200

    actualizacion de codigos c array

--- array_code/impl3/c_code/array.c
+++ array_code/impl3/c_code/array.c
@@ -3,7 +3,7 @@
 int main()
 {
   // size of the array
-  int size =16;
+  int size =128;
 
   //make arrays
   float a[size][size], b[size][size], result[size][size];
@@ -12,25 +12,30 @@ int main()
   int i,j;
   printf("Enter elements of 1st matrix\n");
   for (i = 0; i < size; ++i) {
-    for (j = 0; j < size; ++j)
+    for (j = 0; j < size; j+=2)
     {
       a[i][j] = i;
+      a[i][j+1] = i;
+      
     }
   }
   
   // Taking input using nested for loop
   for (i = 0; i < size; ++i) {
-    for (j = 0; j < size; ++j)
+    for (j = 0; j < size; j+=2)
     {
       b[i][j] = i;
+      b[i][j+1] = i;
     }
   }
   
   // multiple corresponding elements of two arrays
   for (i = 0; i < size; ++i) {
-    for (j = 0; j < size; ++j)
+    for (j = 0; j < size; j+=2)
     {
+    
       result[i][j] = a[i][j] * b[i][j];
+      result[i][j+1] = a[i][j+1] * b[i][j+1];
     }
   }
 
