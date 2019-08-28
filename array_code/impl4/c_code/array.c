22e9dd911b28fa0c8e1ef7f3726099e1b9497e19
Ocram55 <marcov2811@gmail.com>
2019-08-28 05:02:31 +0200

    finalizacion codigos array

--- array_code/impl4/c_code/array.c
+++ array_code/impl4/c_code/array.c
@@ -3,38 +3,35 @@
 int main()
 {
   // size of the array
-  int size =16;
+  int size =128;
 
   //make arrays
   float a[size][size], b[size][size], result[size][size];
   
   // Taking input using nested for loop
-  int i1,j1,i2,j2;
+  int ii,jj,i,j;
   int t = 4;
   printf("Enter elements of 1st matrix\n");
-  for (i1 = 0; i1 < size; i1+=t) {
-    for (j1 = 0; j1 < size; j1+=t){
-	 for (i2 = i1; i2 < i1+t-1; i2++) {
-	    for (j2 = j1; j1 < j1+t-1; j2++){
-      a[i2][j2] = i1;
-      b[i2][j2] = i1;
+  for (ii = 0; ii < size; ii+=t) {
+    for (jj = 0; jj < size; jj+=t){
+	 for (i = ii; i < ii+t; i++) {
+	    for (j = jj; j < jj+t; j++){
+     		 a[i][j] = i;
+     		 b[i][j] = i;
     }
   }
   }
 }
    
   // multiple corresponding elements of two arrays
-  for (i1 = 0; i1 < size; i1+=t) {
-    for (j1 = 0; j1 < size; j1+=t){
-	 for (i2 = i1; i2 < i1+t-1; i2++) {
-	    for (j2 = j1; j1 < j1+t-1; j2++){
-      		result[i2][j2] = a[i2][j2] * b[i2][j2];
+  for (ii = 0; ii < size; ii+=t) {
+    for (jj = 0; jj < size; jj+=t){
+	 for (i = ii; i < ii+t; i++) {
+	    for (j = jj; j < jj+t; j++){
+              result[i][j] = a[i][j] * b[i][j];
     }
   }
-  }
 }
-     
- 
-
+}
   return 0;
 }
