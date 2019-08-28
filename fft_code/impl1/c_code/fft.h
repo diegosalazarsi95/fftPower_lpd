b9276470acb4cdd0db2c900ce351c951cafbb7b4
diegosalazarsi95 <diegosalazarsi95@gmail.com>
2019-08-28 03:51:09 +0200

    Codigos FFT actualizados-- Impl1 Good-Thomas FFT -- Impl2 Cooley-Tuckey FFT -- Impl3 NO DEFINED

---
+++ fft_code/impl1/c_code/fft.h
@@ -0,0 +1,4 @@
+#include "complex.h"
+
+complex* FFT_GoodThomas(complex* x, int N, int N1, int N2);
+complex* DFT_naive(complex* x, int N);
---
+++ fft_code/impl1/c_code/fft_test.c
@@ -0,0 +1,22 @@
+#include "fft.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    complex * input1 = (complex*) malloc(sizeof(struct complex_t) * 30);
+    complex * input2 = (complex*) malloc(sizeof(struct complex_t) * 30);
+    complex * result1;
+    
+    /* Init inputs */
+    for (int i=0; i < 30; i++) {
+        input1[i].re = (double) i;
+        input1[i].im = 0.0;
+        input2[i].re = (double) i;
+        input2[i].im = 0.0;
+    }
+    
+    /* Do FFT */
+    result1 = FFT_GoodThomas(input2, 30, 6, 5);
+    
+    return 0;
+}
