bde84ac618556ed49913cab6c4ebbda796abcbd7
Diego Salazar <34428910+diegosalazarsi95@users.noreply.github.com>
2019-08-29 00:44:41 -0600

    FFTSimple for impl3

--- fft_code/impl3/c_code/fft_v2.c
+++ fft_code/impl3/c_code/fft_v2.c
@@ -1,108 +1,91 @@
-/*
- * fft() - a straightforward implementation of a decimation in time
- *         FFT algorithm.  No fancy micro-twiddling of C code.  Let
- *         the compiler do that for you.
- *
- * input  - Pointer to the input vector.
- * output - Pointer to where you want the output, ie this is not an
- *          in-place algorithm.
- * n      - The number of points in the input vector.  Must be a power
- *          of two.
- * dir    - Direction :  1 = forward
- *                      -1 = reverse
- * 
- * See Cormen, Leiserson, and Rivest's _Introduction_to_Algorithms_,
- * Chapter 32 for more details.
- * 
- * If you need to speed this up, rewrite bit_reverse_copy() and try changing
- * the local variables with complex type into two variables with double type
- * Some uses of const may help, too.  YMMV.
- *
- * Matt Donadio - m.p.donadio@ieee.org
- *
- */
+/* The authors of this work have released all rights to it and placed it
+in the public domain under the Creative Commons CC0 1.0 waiver
+(http://creativecommons.org/publicdomain/zero/1.0/).
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+Retrieved from: http://en.literateprograms.org/Cooley-Tukey_FFT_algorithm_(C)?oldid=19032
+*/
 
-#include <stdio.h>
+
 #include <stdlib.h>
 #include <math.h>
 
-/* It's not there on mingw systems [05/01 - sof] */
-#ifndef M_PI
-#define M_PI		3.14159265358979323846
-#endif
+#define PI  3.1415926535897932
 
 typedef struct complex_t {
-  double r;
-  double i;
+    double re;
+    double im;
 } complex;
 
-static void bit_reverse_copy(complex *, complex *, int);
-
-void fft(complex * input, complex * output, unsigned int n, int dir)
-{
-  unsigned int s, j, k;
-  unsigned int lgn;
-  unsigned int m;
-  complex Wm, W;
-  complex t, u;
-
-  bit_reverse_copy(input, output, n);
-
-  for (lgn = 0; n >> lgn > 1; lgn++);
-
-  for (s = 1; s <= lgn; s++) {
-    m = 1 << s;
-    Wm.r = cos(2.0 * M_PI / (double) m);
-    Wm.i = sin(-dir * 2.0 * M_PI / (double) m);
-    W.r = 1.0;
-    W.i = 0.0;
-    for (j = 0; j <= m / 2 - 1; j++) {
-      for (k = j; k <= n - 1; k += m) {
-	t.r = W.r * (output + k + m / 2)->r - W.i * (output + k + m / 2)->i;
-	t.i = W.r * (output + k + m / 2)->i + W.i * (output + k + m / 2)->r;
-	u.r = (output + k)->r;
-	u.i = (output + k)->i;
-	(output + k)->r = u.r + t.r;
-	(output + k)->i = u.i + t.i;
-	(output + k + m / 2)->r = u.r - t.r;
-	(output + k + m / 2)->i = u.i - t.i;
-      }
-      t.r = W.r * Wm.r - W.i * Wm.i;
-      t.i = W.r * Wm.i + W.i * Wm.r;
-      W.r = t.r;
-      W.i = t.i;
-    }
-  }
-
-  if (dir == -1) {
-    for (j = 0; j < n; j++) {
-      (output + j)->r /= (double) n;
-      (output + j)->i /= (double) n;
-    }
-  }
+complex complex_from_polar(double r, double theta_radians) {
+    complex result;
+    result.re = r * cos(theta_radians);
+    result.im = r * sin(theta_radians);
+    return result;
 }
 
-/*
- * this could probably be a lot better...
- */
+complex complex_add(complex left, complex right) {
+    complex result;
+    result.re = left.re + right.re;
+    result.im = left.im + right.im;
+    return result;
+}
 
-static void bit_reverse_copy(complex * input, complex * output, int n)
-{
-  unsigned int i, j, k;
-  int lgn;
+complex complex_sub(complex left, complex right) {
+    complex result;
+    result.re = left.re - right.re;
+    result.im = left.im - right.im;
+    return result;
+}
 
-  for (lgn = 0; n >> lgn > 1; lgn++);
+complex complex_mult(complex left, complex right) {
+    complex result;
+    result.re = left.re*right.re - left.im*right.im;
+    result.im = left.re*right.im + left.im*right.re;
+    return result;
+}
 
-  for (i = 0; i < n; i++) {
-    k = 0;
-    for (j = 0; j < lgn; j++) {
-      if ((i & (1 << j)) == (1 << j)) {
-	k += 1 << (lgn - 1 - j);
-      }
+complex* FFT_simple(complex* x, int N /* must be a power of 2 */) {
+    complex* X = (complex*) malloc(sizeof(struct complex_t) * N);
+    complex * d, * e, * D, * E;
+    int k;
+
+    if (N == 1) {
+        X[0] = x[0];
+        return X;
     }
-    (output + i)->r = (input + k)->r;
-    (output + i)->i = (input + k)->i;
-  }
+
+    e = (complex*) malloc(sizeof(struct complex_t) * N/2);
+    d = (complex*) malloc(sizeof(struct complex_t) * N/2);
+    for(k = 0; k < N/2; k++) {
+        e[k] = x[2*k];
+        d[k] = x[2*k + 1];
+    }
+
+    E = FFT_simple(e, N/2);
+    D = FFT_simple(d, N/2);
+
+    for(k = 0; k < N/2; k++) {
+        /* Multiply entries of D by the twiddle factors e^(-2*pi*i/N * k) */
+        complex c = complex_from_polar(1, -2.0*PI*k/N);
+        D[k] = complex_mult(D[k], c);
+    }
+
+    for(k = 0; k < N/2; k++) {
+        X[k]       = complex_add(E[k], D[k]);
+        X[k + N/2] = complex_sub(E[k], D[k]);
+    }
+
+    free(e);
+    free(d);
+    free(D);
+    free(E);
+    return X;
 }
 
 int main(void) {
@@ -118,7 +101,7 @@ int main(void) {
     }
     
     /* Do FFT */
-    fft(input1, result1, N, 1);
+    result1 = FFT_simple(input1, N);
     
     return 0;
 }
